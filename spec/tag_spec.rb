1dff9b61e21759d35e0ef1f16a00b7fd7f0e5ffb
Travis Crampton & Christoph Pirringer <student@epicodus.com>
2015-08-27 10:15:06 -0700

    add tag, recipe, and ingredient specs

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,13 +1,5 @@
 require('spec_helper')
 
 describe(Tag) do
-  it("belongs to a recipe") do
-    test_recipe = Recipe.create({:name => "Sausage Delight"})
-    test_tag1 = Tag.create({:description => "Hearty Food", :recipe_id => test_recipe.id})
-    expect(test_recipe.tags()).to(eq([test_tag1]))
-  end
-
-  describe(Tag) do
     it {should have_and_belong_to_many(:recipes)}
-  end
 end
